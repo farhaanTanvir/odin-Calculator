019d03b647b460313d221895eab580360f8b4892
Farhan Tanvir <ftanvir723@gmail.com>
2026-03-10 16:52:29 +0600

    Completed basic calculation function

--- script.js
+++ script.js
@@ -30,18 +30,82 @@ const numberContainer = document.querySelector('.container')
 
 let firstNumber = "";
 let operator;
-let lastNumber;
+let lastNumber = "";
 
-function loadScreenFirst(num) {
+let conditionMovedOn = false;
+
+const ops = ["add", "subtract", "multiply", "divide"]
+
+function loadScreen(num) {
     display.innerText = num;
 }
 
-numberContainer.addEventListener('click', (e) => {
+
+function handleFirstInput(e) {
     e.preventDefault();
     if (!e.target.classList.contains('btn-number')) {
         return;
     }
     const number = Number(e.target.textContent);
     firstNumber += `${number}`;
-    loadScreenFirst(firstNumber);
-});
+    loadScreen(firstNumber);
+    return firstNumber;
+}
+
+numberContainer.addEventListener('click', handleFirstInput)
+
+function switchToSecondVal() {
+    numberContainer.removeEventListener('click', handleFirstInput)
+    numberContainer.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (!e.target.classList.contains('btn-number')) {
+            return;
+        }
+        const number = Number(e.target.textContent);
+        lastNumber += `${number}`;
+        loadScreen(lastNumber);
+        return lastNumber;
+    });
+}
+
+btnAdd.addEventListener('click', () => {
+    if (firstNumber === "") {
+        return;
+    }
+    operator = 0;
+    switchToSecondVal();
+    return operator;
+
+})
+btnSubtract.addEventListener('click', () => {
+    if (firstNumber === "") {
+        return;
+    }
+    operator = 1;
+    switchToSecondVal();
+    return operator
+
+})
+btnMultiply.addEventListener('click', () => {
+    if (firstNumber === "") {
+        return;
+    }
+    operator = 2;
+    switchToSecondVal();
+    return operator
+})
+btnDivide.addEventListener('click', () => {
+    if (firstNumber === "") {
+        return;
+    }
+    operator = 3;
+    switchToSecondVal();
+    return operator
+})
+
+
+btnEquals.addEventListener("click", () => {
+    const operatorArr = [function add() { return Number(firstNumber) + Number(lastNumber) }, function subtract() { return Number(firstNumber) - Number(lastNumber) }, function multiply() { return Number(firstNumber) * Number(lastNumber) }, function divide() { return Number(firstNumber) / Number(lastNumber) }];
+    let result = operatorArr[operator]();
+    loadScreen(result);
+})
