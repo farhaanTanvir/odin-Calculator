db644a3b42141128d56c08d39f47426e5531fb7a
Farhan Tanvir <ftanvir723@gmail.com>
2026-03-10 13:53:41 +0600

    prepared the skeleton

---
+++ script.js
@@ -0,0 +1,33 @@
+const display = document.querySelector('#current');
+const expression = document.querySelector('#expression');
+
+const btnZero = document.querySelector('#zero');
+const btnOne = document.querySelector('#one');
+const btnTwo = document.querySelector('#two');
+const btnThree = document.querySelector('#three');
+const btnFour = document.querySelector('#four');
+const btnFive = document.querySelector('#five');
+const btnSix = document.querySelector('#six');
+const btnSeven = document.querySelector('#seven');
+const btnEight = document.querySelector('#eight');
+const btnNine = document.querySelector('#nine');
+const btnDecimal = document.querySelector('#decimal');
+
+const btnAdd = document.querySelector('#add');
+const btnSubtract = document.querySelector('#subtract');
+const btnMultiply = document.querySelector('#multiply');
+const btnDivide = document.querySelector('#divide');
+
+const btnEquals = document.querySelector('#equals');
+const btnClear = document.querySelector('#clear');
+const btnDelete = document.querySelector('#delete');
+
+
+// ALL THE BUTTONS READY
+
+
+let firstNumber;
+let operator;
+let lastNumber;
+
+
